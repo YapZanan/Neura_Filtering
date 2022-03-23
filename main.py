01e2c8bff36f81a7c0da8556281c5dd07cbdc2d9
YapZanan <naufalsetiawan64@gmail.com>
2022-03-23 21:25:02 +0700

    contoh

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
+import os
 from threading import *
 from PyQt5 import QtWidgets, uic
 import sys
-from PyQt5.QtWidgets import QTableWidgetItem, QListWidgetItem, QDialog
+from PyQt5.QtWidgets import QTableWidgetItem, QListWidgetItem, QDialog, QFileDialog
+
 
 class Ui(QtWidgets.QMainWindow):
     def __init__(self):
         super(Ui, self).__init__()
         uic.loadUi('main.ui', self)
+        self.fname = None
+        self.pushButton_Load.clicked.connect(self.loadfileketabel)
+
+
+    def loadfileketabel(self):
+        self.fname = QFileDialog.getExistingDirectory(self, 'Load File')
+        if self.fname.isEmpty():
+            print("wuhu")
+        else:
+            print(self.fname[0])
+            print("aaa")
+
+    def thread_load(self):
+        t1 = Thread(target=self.loadfileketabel)
+        t1.start()
+
 
 
 app = QtWidgets.QApplication(sys.argv)
