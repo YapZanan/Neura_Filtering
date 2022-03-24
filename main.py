7bcdbde744b209c88f0734495fb2c5236a491c52
YapZanan <naufalsetiawan64@gmail.com>
2022-03-24 07:41:32 +0700

    fitur auto load data ke tabel

--- main.py
+++ main.py
@@ -9,18 +9,33 @@ class Ui(QtWidgets.QMainWindow):
     def __init__(self):
         super(Ui, self).__init__()
         uic.loadUi('main.ui', self)
-        self.fname = None
+
+        self.path = None
+        self.data = None
+
+
         self.pushButton_Load.clicked.connect(self.loadfileketabel)
 
 
+    def bacafolder(self):
+        #, options= QFileDialog.DontUseNativeDialog
+        path = QFileDialog.getExistingDirectory(self, 'Load File')
+        return path
+
     def loadfileketabel(self):
-        self.fname = QFileDialog.getExistingDirectory(self, 'Load File')
-        if self.fname.isEmpty():
-            print("wuhu")
-            print("eca")
+        self.path = self.bacafolder()
+        if self.path == '':
+            print("Tidak memilih file")
         else:
-            print(self.fname[0])
-            print("aaa")
+            baca = os.listdir(self.path)
+            self.data = [s.replace(".jpeg", "") for s in baca]
+            total = len(self.data)
+            self.tableWidget_Tabel_File.setRowCount(total)
+            for i in range(total):
+                self.tableWidget_Tabel_File.setItem(i, 0, QTableWidgetItem(str(self.data[i])))
+                # self.Tabel_Mahasiswa.setItem(i, 1, QTableWidgetItem(str(password[i])))
+
+
 
     def thread_load(self):
         t1 = Thread(target=self.loadfileketabel)
