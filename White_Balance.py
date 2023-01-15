8334f3ec8dab357016fafa94cc95168e2e1db0c3
YapZanan <naufalsetiawan64@gmail.com>
2023-01-15 10:04:09 +0700

    update

--- White_Balance.py
+++ White_Balance.py
@@ -1,5 +1,5 @@
 import cv2
-import numpy
+import numpy as np
 import os
 from skimage import exposure
 from skimage.exposure import match_histograms
@@ -12,6 +12,32 @@ class White_Balance():
         """ Split gambar, average nilai, kembalikan nilai ke R G B
         https://docs.opencv.org/3.4/d7/d71/classcv_1_1xphoto_1_1GrayworldWB.html
         """
+
+    def histogram_matching(self):
+        matched = match_histograms(image, reference, channel_axis=-1)
+
+    def retinex_adjust(nimg):
+        """
+        from 'Combining Gray World and Retinex Theory for Automatic White Balance in Digital Photography'
+        """
+        nimg = nimg.transpose(2, 0, 1).astype(np.uint32)
+        sum_r = np.sum(nimg[0])
+        sum_r2 = np.sum(nimg[0] ** 2)
+        max_r = nimg[0].max()
+        max_r2 = max_r ** 2
+        sum_g = np.sum(nimg[1])
+        max_g = nimg[1].max()
+        coefficient = np.linalg.solve(np.array([[sum_r2, sum_r], [max_r2, max_r]]),
+                                      np.array([sum_g, max_g]))
+        nimg[0] = np.minimum((nimg[0] ** 2) * coefficient[0] + nimg[0] * coefficient[1], 255)
+        sum_b = np.sum(nimg[1])
+        sum_b2 = np.sum(nimg[1] ** 2)
+        max_b = nimg[1].max()
+        max_b2 = max_r ** 2
+        coefficient = np.linalg.solve(np.array([[sum_b2, sum_b], [max_b2, max_b]]),
+                                      np.array([sum_g, max_g]))
+        nimg[1] = np.minimum((nimg[1] ** 2) * coefficient[0] + nimg[1] * coefficient[1], 255)
+        return nimg.transpose(1, 2, 0).astype(np.uint8)
 
     def kefoto(self):
         test =  os.path.isdir("venv")
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import os
-from threading import *
-from PyQt5 import QtWidgets, uic, QtGui
+from threading import Thread
+from PyQt5 import QtWidgets, uic
 import sys
-
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap, QImage
-from PyQt5.QtWidgets import QTableWidgetItem, QListWidgetItem, QDialog, QFileDialog
+from PyQt5.QtWidgets import QTableWidgetItem, QDialog, QFileDialog
 import White_Balance
 
 class Ui(QtWidgets.QMainWindow):
@@ -18,56 +17,50 @@ class Ui(QtWidgets.QMainWindow):
         self.data = None
         self.image = QImage()
 
-        self.pushButton_Load.clicked.connect(self.loadfileketabel)
-
-
-        # self.tableWidget_Tabel_File.clicked.connect(self.loadkelabel)
-
-        self.tableWidget_Tabel_File.clicked.connect(self.wba)
-        # self.checkBox_Vignette.stateChanged.connect(self.wba)
-
+        self.pushButton_Load.clicked.connect(self.load_file_to_table)
+        self.tableWidget_Tabel_File.clicked.connect(self.load_image_to_label)
 
 
     def wba(self):
-        self.wb.grayworld(self.loadkelabel(), len(self.data))
-        # print("aaa")
-
-    def bacafolder(self):
-        #, options= QFileDialog.DontUseNativeDialog
+        # aa = self.wb.retinex_adjust(self.get_selected_file_path())
+        # self.image = QImage(aa, aa.shape[1], aa.shape[0], QImage.Format_RGB888)
+        pass
+    def select_folder(self):
         path = QFileDialog.getExistingDirectory(self, 'Load File')
         return path
 
-
-    def loadkelabel(self):
+    def get_selected_file_path(self):
         row = self.tableWidget_Tabel_File.currentRow()
         data = self.tableWidget_Tabel_File.item(row, 0)
         text = data.text()
         path_lengkap = self.path + "/" + str(text)
-        # print(self.path_lengkap)
         return path_lengkap
-        # self.label_Camera.setPixmap(QtGui.QPixmap(path))
-        # self.label_Camera.setScaledContents(True)
 
-
-    def loadfileketabel(self):
-        self.path = self.bacafolder()
+    def load_file_to_table(self):
+        self.path = self.select_folder()
         if self.path == '':
-            print("Tidak memilih file")
+            print("No file selected")
         else:
             baca = os.listdir(self.path)
-            self.data = [s.replace(".jpeg", "") for s in baca]
+            self.data = []
+            for file in baca:
+                if file.endswith(".jpeg") or file.endswith(".jpg") or file.endswith(".png") or file.endswith(
+                        ".bmp") or file.endswith(".gif"):
+                    self.data.append(file.replace(".jpeg", ""))
             total = len(self.data)
             self.tableWidget_Tabel_File.setRowCount(total)
             for i in range(total):
                 self.tableWidget_Tabel_File.setItem(i, 0, QTableWidgetItem(str(self.data[i])))
-                # self.Tabel_Mahasiswa.setItem(i, 1, QTableWidgetItem(str(password[i])))
-
-
+            self.label_Camera.clear()
 
     def thread_load(self):
-        t1 = Thread(target=self.loadfileketabel)
+        t1 = Thread(target=self.load_file_to_table)
         t1.start()
 
+    def load_image_to_label(self):
+        path = self.get_selected_file_path()
+        self.label_Camera.setPixmap(QPixmap(path))
+        self.label_Camera.setScaledContents(True)
 
 
 app = QtWidgets.QApplication(sys.argv)
